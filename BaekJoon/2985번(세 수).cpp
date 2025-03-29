41377b0f6ac087abbc5e9ea3c6c8f1f16385df5e
seeum0507 <kwonstella0507@gmail.com>
2025-03-29 23:27:26 +0900

    백준 2985번

---
+++ BaekJoon/2985번(세 수).cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b == c) {
+        printf("%d+%d=%d\n", a, b, c);
+    }
+    else if (a - b == c) {
+        printf("%d-%d=%d\n", a, b, c);
+    }
+    else if (a * b == c) {
+        printf("%d*%d=%d\n", a, b, c);
+    }
+    else if (a / b == c) {
+        printf("%d/%d=%d\n", a, b, c);
+    }
+    else if (a == b + c) {
+        printf("%d=%d+%d\n", a, b, c);
+    }
+    else if (a == b - c) {
+        printf("%d=%d-%d\n", a, b, c);
+    }
+    else if (a == b * c) {
+        printf("%d=%d*%d\n", a, b, c);
+    }
+    else if (a == b / c) {
+        printf("%d=%d/%d\n", a, b, c);
+    }
+
+}
