8ed0df8fc31e33388688dbc394991f69aca8f429
권세은 <163130138+seeum0507@users.noreply.github.com>
2025-04-24 19:11:40 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int hour, minute;
+
+    scanf("%d %d", &hour, &minute);
+
+    minute = minute - 45;
+
+    if (minute < 0) {
+        minute = minute + 60;
+        hour = hour - 1;
+
+        if (hour < 0) {
+            hour = 23;
+        }
+    }
+
+    printf("%d %d\n", hour, minute);
+    return 0;
+}
